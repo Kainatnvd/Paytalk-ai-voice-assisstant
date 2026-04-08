02bda55309f9250bc4b9709b4e380b418bfdc56f
Kainat_nvd <kainat22nvd@gmail.com>
2026-04-08 05:03:04 +0500

    Phase 3 - RASA is done
    only wiring to frontend is left otherwise rasa is 100% done.
    also testing for whisper is done as well.

---
+++ backend/alembic/versions/522786ba7997_merge_migrations.py
@@ -0,0 +1,27 @@
+"""merge migrations
+
+Revision ID: 522786ba7997
+Revises: 139261da4f89, 8b4171408110
+Create Date: 2026-03-26 01:45:32.945877
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '522786ba7997'
+down_revision: Union[str, Sequence[str], None] = ('139261da4f89', '8b4171408110')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
